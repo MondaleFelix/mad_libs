842d2f66d7d11d842ddb4cef51aa555b981838ad
MondaleFelix <mondalefelix@yahoo.com>
2019-08-29 13:45:15 -0700

    modularized my code

--- mad_libs.py
+++ mad_libs.py
@@ -11,21 +11,29 @@ title = data["title"]
 template = data["value"]
 blanks = data["blanks"]
 
+def ascii_art(phrase):
+	subprocess.call("figlet " + phrase, shell=True)
 
-subprocess.call("figlet " + title, shell=True)
+ascii_art(title)
+
+
 
 user_inputs = []
+get_user_inputs()
 
-for blank in blanks:
-	user_input = input("Enter " + blank + ": ")
-	user_inputs.append(user_input)
+def get_user_inputs():
+	for blank in blanks:
+		user_input = input("Enter " + blank + ": ")
+		user_inputs.append(user_input)
 
 
+fill_in_blanks()
 
-counter = 1
-for i in range(len(user_inputs)):
-	template.insert(i + counter ,user_inputs[i])
-	counter += 1 
+def fill_in_blanks():
+	counter = 1
+	for i in range(len(user_inputs)):
+		template.insert(i + counter ,user_inputs[i])
+		counter += 1 
 
 
 
