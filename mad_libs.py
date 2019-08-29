b2fdfdf17638765c683b1af802b84d86ed9ae2ca
MondaleFelix <mondalefelix@yahoo.com>
2019-08-29 13:31:03 -0700

    retrieve from api

--- mad_libs.py
+++ mad_libs.py
@@ -1,10 +1,34 @@
 import json 
+import subprocess
 import requests
 
 api_url = "http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25"
 
-r = requests.get(api_url)
+r = requests.get(api_url)	
+data = r.json()
 
-data = r.json
+title = data["title"]
+template = data["value"]
+blanks = data["blanks"]
 
-print(data)
+
+subprocess.call("figlet " + title, shell=True)
+
+user_inputs = []
+
+for blank in blanks:
+	user_input = input("Enter " + blank + ": ")
+	user_inputs.append(user_input)
+
+
+
+counter = 1
+for i in range(len(user_inputs)):
+	template.insert(i + counter ,user_inputs[i])
+	counter += 1 
+
+
+
+
+print(template)
+
