78ae653014a94cc65bad5c5ac61fa31b0bfe92ee
MondaleFelix <mondalefelix@yahoo.com>
2019-09-01 16:52:28 -0700

    added comments

--- mad_libs.py
+++ mad_libs.py
@@ -7,22 +7,23 @@ api_url = "http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25"
 r = requests.get(api_url)	
 data = r.json()
 
+# JSON Object 
 title = data["title"]
 template = data["value"][:-1]
 blanks = data["blanks"]
 user_inputs = []
 
-
+# Creates ascii art 
 def ascii_art(phrase):
 	subprocess.call("figlet " + phrase, shell=True)
 
-
+# Gets user input for every blank.
 def get_user_inputs():
 	for blank in blanks:
 		user_input = input("Enter " + blank + ": ")
 		user_inputs.append(user_input)
 
-
+# Fills in template with the collected user inputs
 def fill_in_blanks():
 	counter = 1
 	for i in range(len(user_inputs)):
